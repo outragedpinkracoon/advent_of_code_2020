df1c244b9a8f468d4d6a4997ffa4ea39491509d8
Valerie Dryden <outragedpinkracoon@gmail.com>
2020-12-06 17:02:47 +0000

    Adds some comments to Day 3 Part 2

--- day_3/part_2.rb
+++ day_3/part_2.rb
@@ -1,7 +1,9 @@
 # frozen_string_literal: true
 
-# Advent of code day 3 part 1 https://adventofcode.com/2020/day/3#part2
+# Advent of code day 3 part 2 https://adventofcode.com/2020/day/3#part2
 module Part2
+  # I've done a disgusting thing here, and just 'skipped' as a quick fix
+  # if the movement is 2 and not 1
   def self.run(slopes, input)
     output = slopes.map do |slope|
       skip = slope[:skip] || false
@@ -12,6 +14,7 @@ module Part2
 
   def self.run_slope(input, movement, skip: false)
     x_position = movement
+    # if we should skip remove all the odd rows from the data
     valid_rows = skip ? input.reject { |v| input.index(v).odd? }.drop(1) : input.drop(1)
     valid_rows.each_with_object({ values: [], count: 0 }) do |row, obj|
       index = x_position % (row.size - 1)
