6d67c10f1ee0103f127b292a45616c52ddf47492
Valerie Dryden <outragedpinkracoon@gmail.com>
2020-12-08 21:55:14 +0000

    Parses sample input for Day 7 Part 1

---
+++ day_7/day_7.rb
@@ -0,0 +1,43 @@
+# frozen_string_literal: true
+
+require 'pry-byebug'
+
+# Advent of code day 7 part 1 https://adventofcode.com/2020/day/7
+module Day7
+  def self.parse(input, result)
+    input.each do |line|
+      parse_line(line, result)
+    end
+    result
+  end
+
+  # Takes one line of input:
+  # "light red bags contain 1 bright white bag, 2 muted yellow bags."
+  # returns a hash with it's parent and children keys added
+  # { light_red: [:bright_white, :muted_yellow] }
+  def self.parse_line(input, result)
+    # ["light red bags", "1 bright white bag, 2 muted yellow bags."]
+    parts = input.split('contain').map(&:strip)
+    # :light_red
+    containing_bag = parts[0].gsub(' bags', '').gsub(' ', '_').to_sym
+    no_child_bags = parts[1].include?('no other bags')
+
+    if no_child_bags
+      result[containing_bag] = []
+      return result
+    end
+
+    result[containing_bag] = child_bags(parts)
+    result
+  end
+
+  def self.child_bags(parts)
+    # ["1 bright white bag", "2 muted yellow bags."]
+    child_bag_parts = parts[1].split(',').map(&:strip)
+    # [:bright_white, :muted_yellow]
+    child_bag_parts.map do |part|
+      _, first, second = part.split(' ')
+      "#{first}_#{second}".to_sym
+    end
+  end
+end
---
+++ day_7/day_7_spec.rb
@@ -0,0 +1,42 @@
+# frozen_string_literal: true
+
+require 'rspec'
+require_relative 'day_7'
+require_relative '../test_helper'
+
+# rubocop:disable Style/SymbolArray
+RSpec.describe do
+  include TestHelper
+
+  it 'can parse a line with child bags' do
+    input = 'light red bags contain 1 bright white bag, 2 muted yellow bags.'
+    expected = {
+      light_red: [:bright_white, :muted_yellow]
+    }
+    expect(Day7.parse_line(input, {})).to eq(expected)
+  end
+
+  it 'can parse a line with no child bags' do
+    input = 'dotted black bags contain no other bags.'
+    expected = {
+      dotted_black: []
+    }
+    expect(Day7.parse_line(input, {})).to eq(expected)
+  end
+
+  it 'parses the sample input' do
+    expected = {
+      light_red: [:bright_white, :muted_yellow],
+      dark_orange: [:bright_white, :muted_yellow],
+      bright_white: [:shiny_gold],
+      muted_yellow: [:shiny_gold, :faded_blue],
+      shiny_gold: [:dark_olive, :vibrant_plum],
+      vibrant_plum: [:faded_blue, :dotted_black],
+      dark_olive: [:faded_blue, :dotted_black],
+      faded_blue: [],
+      dotted_black: []
+    }
+    expect(Day7.parse(File.readlines('sample.txt'), {})).to eq(expected)
+  end
+end
+# rubocop:enable Style/SymbolArray
