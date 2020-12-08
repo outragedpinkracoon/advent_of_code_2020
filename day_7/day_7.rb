d92866658ef482dc88a25629424e2c92253c19ea
Valerie Dryden <outragedpinkracoon@gmail.com>
2020-12-08 22:59:16 +0000

    Day 7 Part 1 I AM A GODDESS

--- day_7/day_7.rb
+++ day_7/day_7.rb
@@ -4,7 +4,11 @@ require 'pry-byebug'
 
 # Advent of code day 7 part 1 https://adventofcode.com/2020/day/7
 module Day7
-  def self.parse(input, result)
+  def self.run(input)
+    count_parents(parse(input))
+  end
+
+  def self.parse(input, result = {})
     input.each do |line|
       parse_line(line, result)
     end
@@ -16,9 +20,8 @@ module Day7
   # returns a hash with it's parent and children keys added
   # { light_red: [:bright_white, :muted_yellow] }
   def self.parse_line(input, result)
-    # ["light red bags", "1 bright white bag, 2 muted yellow bags."]
     parts = input.split('contain').map(&:strip)
-    # :light_red
+
     containing_bag = parts[0].gsub(' bags', '').gsub(' ', '_').to_sym
     no_child_bags = parts[1].include?('no other bags')
 
@@ -31,25 +34,38 @@ module Day7
     result
   end
 
+  # Takes in the child info parts e.g ["1 bright white bag", "2 muted yellow bags."]
+  # returns the names as a list of symbols # [:bright_white, :muted_yellow]
   def self.child_bags(parts)
-    # ["1 bright white bag", "2 muted yellow bags."]
     child_bag_parts = parts[1].split(',').map(&:strip)
-    # [:bright_white, :muted_yellow]
     child_bag_parts.map do |part|
       _, first, second = part.split(' ')
       "#{first}_#{second}".to_sym
     end
   end
 
-  def self.count_parents(input)
-    search(input.keys, input)
+  def self.count_parents(bag_data)
+    result = []
+    # loop over each parent bag
+    bag_data.each_key do |current_bag_name|
+      child_bags = bag_data[current_bag_name]
+      search(bag_data, current_bag_name, result, child_bags)
+    end
+    # don't double count when a parent back has multiple children
+    # that can hold a shiny gold bag
+    result.uniq.count
   end
 
-  def self.search(keys, input, result = 0)
-    return result if keys.empty?
-
-    current_bag_children = input[keys.first]
-    result += 1 if current_bag_children.include?(:shiny_gold)
-    search(keys.drop(1), input, result)
+  # loop over the child bags and check if they are a shiny gold bag,
+  # if not, recurse on the remaining children and check them
+  def self.search(bag_data, key, result, child_bags)
+    child_bags.each do |child|
+      if child != :shiny_gold
+        # check if the children of the child can hold a shiny gold bag
+        search(bag_data, key, result, bag_data[child])
+      else
+        result << key
+      end
+    end
   end
 end
--- day_7/day_7_spec.rb
+++ day_7/day_7_spec.rb
@@ -39,7 +39,7 @@ RSpec.describe do
     expect(Day7.parse(File.readlines('sample.txt'), {})).to eq(expected)
   end
 
-  xit 'passes with only one direct parent' do
+  it 'passes with only one direct parent' do
     input = { bright_white: [:shiny_gold] }
     expect(Day7.count_parents(input)).to eq(1)
   end
@@ -47,9 +47,53 @@ RSpec.describe do
   it 'passes with two direct parents' do
     input = {
       bright_white: [:shiny_gold],
-      muted_yellow: [:shiny_gold, :faded_blue]
+      muted_yellow: [:shiny_gold, :faded_blue],
+      faded_blue: []
     }
     expect(Day7.count_parents(input)).to eq(2)
+  end
+
+  it 'passes when doing multiple hops' do
+    input = {
+      dark_orange: [:bright_white, :muted_yellow],
+      bright_white: [:shiny_gold],
+      muted_yellow: [],
+      shiny_gold: []
+    }
+    expect(Day7.count_parents(input)).to eq(2)
+  end
+
+  it 'passes when both children can contain the gold bag' do
+    input = {
+      light_red: [:bright_white, :muted_yellow],
+      muted_yellow: [:shiny_gold],
+      bright_white: [:shiny_gold],
+      shiny_gold: []
+    }
+    expect(Day7.count_parents(input)).to eq(3)
+  end
+
+  it 'passes with the sample input' do
+    input = {
+      light_red: [:bright_white, :muted_yellow],
+      dark_orange: [:bright_white, :muted_yellow],
+      bright_white: [:shiny_gold],
+      muted_yellow: [:shiny_gold, :faded_blue],
+      shiny_gold: [:dark_olive, :vibrant_plum],
+      vibrant_plum: [:faded_blue, :dotted_black],
+      dark_olive: [:faded_blue, :dotted_black],
+      faded_blue: [],
+      dotted_black: []
+    }
+    expect(Day7.count_parents(input)).to eq(4)
+  end
+
+  it 'passes with the sample input' do
+    expect(Day7.run(File.readlines('sample.txt'))).to eq(4)
+  end
+
+  it 'passes with the actual input' do
+    expect(Day7.run(File.readlines('actual.txt'))).to eq(205)
   end
 end
 # rubocop:enable Style/SymbolArray
